33b67a49447a77a42e751d5c3eb995cfda373ff7
davyusow <dayvsonf27@gmail.com>
2025-03-28 02:07:48 -0300

    correção do desafio 33

--- Aulas/Aula10/desafio33.py
+++ Aulas/Aula10/desafio33.py
@@ -4,19 +4,29 @@ n1 = int(input('Digite o primeiro número: '))
 n2 = int(input('Digite o segundo número: '))
 n3 = int(input('Digite o terceiro número: '))
 
+maior = 0
+menor = 0
 if(n1>n2):
     if(n1>n3):
-        print('{} é o maior'.format(n1))
+        maior = n1
     else:
-        print('{} é o maior'.format(n3))
-else:
-    print('{} é maior'.format(n2))
-
-if(n3<n2):
-    if(n3<n1):
-        print('{} é o menor'.format(n3))
+        maior = n3
+if(n2>n1):
+    if(n2>n3):
+        maior = n2
     else:
-        print('{} é o menor'.format(n1))
-else:
-    print('{} é menor'.format(n2))
+        maior = n3
+    
 
+if(n1<n2):
+    if(n1<n3):
+        menor = n1
+    else:
+        menor = n3
+if(n2<n1):
+    if(n2<n3):
+        menor = n2
+    else:
+        menor = n3
+
+print('{} é o maior, e o {} é o menor'.format(maior,menor))
