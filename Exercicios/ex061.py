c31a4f85a4411c32da7b02b69feeedb5c186ea4e
davyusow <dayvsonf27@gmail.com>
2025-04-01 19:35:53 -0300

    Correção do exercício 61

--- Exercicios/ex061.py
+++ Exercicios/ex061.py
@@ -3,17 +3,10 @@
 n1 = int(input('Digite o primeiro termo da PA: '))
 razao = int(input('Digite a razão da PA: '))
 
-cont = 0
-tam = 9
+cont = 1
 
-while(cont<tam):
-    if(cont == tam-2):
-        print('{} '.format(n1+razao),end='')
-    else:
-        print('{} '.format(n1+razao),end=' → ')
+while(cont<9):
+    print('{} '.format(n1+razao),end=' → ')
     n1+=razao
     cont+=1
-    if(cont == tam-1):
-        tam = int(input('\nDeseja ver mais algum termo? se sim quantos? '))
-        if(tam != 0): tam = cont + tam + 1
-print('Fim')
+print('{} '.format(n1+razao))
