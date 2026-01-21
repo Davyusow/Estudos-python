ad2d839f9adaac54cd26da70827487450f2b264a
vladmir-DEV-code <vladmirlimadasilva506@gmail.com>
2026-01-21 16:18:42 -0300

    COLOQUEI A NOVA FORMA DE FORMATAÇÃO !!!

--- Aulas/Aula07/aula08.py
+++ Aulas/Aula07/aula08.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 
-nome = input('Qual o seu nome?\n')
-print('Prazer em te conhecer {:20}!'.format(nome)) #Nome escrito em 20 espaços
-print('Prazer em te conhecer {:>20}!'.format(nome)) #nome alinhado a direita em 20 espaços
-print('Prazer em te conhecer {:<20}!'.format(nome)) #nome alinhado a esquerda em 20 espaços
-print('Prazer em te conhecer {:^20}!'.format(nome)) #nome alinhado no meio em 20 espaços 
-print('Prazer em te conhecer {:=^20}!'.format(nome)) #nome alinhado no meio em 20 espaços com '=' em volta
+nome = input("Qual o seu nome?\n")
+print("Prazer em te conhecer {:20}!".format(nome))  # Nome escrito em 20 espaços
+print(
+    "Prazer em te conhecer {:>20}!".format(nome)
+)  # nome alinhado a direita em 20 espaços
+print(
+    "Prazer em te conhecer {:<20}!".format(nome)
+)  # nome alinhado a esquerda em 20 espaços
+print(
+    "Prazer em te conhecer {:^20}!".format(nome)
+)  # nome alinhado no meio em 20 espaços
+print(
+    "Prazer em te conhecer {:=^20}!".format(nome)
+)  # nome alinhado no meio em 20 espaços com '=' em volta
+print(f"")  # NOVA FORMA DAVYUSON!!!!!!!!!
